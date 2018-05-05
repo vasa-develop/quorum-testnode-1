18fbfdd01b4336a678c5188bdc94fececacf78ba
vasa <vasa.develop@gmail.com>
2018-05-06 00:15:20 +0530

    added variables PEER1_IP, PEER2_IP

--- raft-init.sh
+++ raft-init.sh
@@ -2,6 +2,15 @@
 set -u
 set -e
 
+PEER1_IP = "18.188.219.84"
+PEER2_IP = "18.222.46.147"
+
+//creating the permissioned-nodes.json
+echo '[
+  "enode://ac6b1096ca56b9f6d004b779ae3728bf83f8e22453404cc3cef16a3d9b96608bc67c4b30db88e0a5a6c6390213f7acbe1153ff6d23ce57380104288ae19373ef@"$PEER1_IP":21000?discport=0&raftport=50401",
+  "enode://3d9ca5956b38557aba991e31cf510d4df641dce9cc26bfeb7de082f0c07abb6ede3a58410c8f249dabeecee4ad3979929ac4c7c496ad20b8cfdd061b7401b4f5@"$PEER2_IP":21003?discport=0&raftport=50404"
+]' > permissioned-nodes.json
+
 echo "[*] Cleaning up temporary data directories"
 rm -rf qdata
 mkdir -p qdata/logs
@@ -13,3 +22,4 @@ cp permissioned-nodes.json qdata/dd1/
 cp keys/key1 qdata/dd1/keystore
 cp raft/nodekey1 qdata/dd1/geth/nodekey
 geth --datadir qdata/dd1 init genesis.json
+
