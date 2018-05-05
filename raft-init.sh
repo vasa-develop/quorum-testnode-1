da4c28f56c092413440165c053e3cbc690369e61
vasa-develop <vasa.develop@gmail.com>
2018-05-06 00:28:09 +0530

    added variables

--- raft-init.sh
+++ raft-init.sh
@@ -2,13 +2,10 @@
 set -u
 set -e
 
-PEER1_IP = "18.188.219.84"
-PEER2_IP = "18.222.46.147"
-
 //creating the permissioned-nodes.json
 echo '[
-  "enode://ac6b1096ca56b9f6d004b779ae3728bf83f8e22453404cc3cef16a3d9b96608bc67c4b30db88e0a5a6c6390213f7acbe1153ff6d23ce57380104288ae19373ef@"'$PEER1_IP'":21000?discport=0&raftport=50401",
-  "enode://3d9ca5956b38557aba991e31cf510d4df641dce9cc26bfeb7de082f0c07abb6ede3a58410c8f249dabeecee4ad3979929ac4c7c496ad20b8cfdd061b7401b4f5@"'$PEER2_IP'":21003?discport=0&raftport=50404"
+  "enode://ac6b1096ca56b9f6d004b779ae3728bf83f8e22453404cc3cef16a3d9b96608bc67c4b30db88e0a5a6c6390213f7acbe1153ff6d23ce57380104288ae19373ef@$1:21000?discport=0&raftport=50401",
+  "enode://3d9ca5956b38557aba991e31cf510d4df641dce9cc26bfeb7de082f0c07abb6ede3a58410c8f249dabeecee4ad3979929ac4c7c496ad20b8cfdd061b7401b4f5@$2:21003?discport=0&raftport=50404"
 ]' > permissioned-nodes.json
 
 echo "[*] Cleaning up temporary data directories"
