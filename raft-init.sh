8d4fd4cb37f6127c84fd2131bd2e167f41694d55
vasa-develop <vasa.develop@gmail.com>
2018-05-07 16:07:05 +0530

    added 3rd node

--- raft-init.sh
+++ raft-init.sh
@@ -6,7 +6,8 @@ set -e
 
 echo '[
   "enode://ac6b1096ca56b9f6d004b779ae3728bf83f8e22453404cc3cef16a3d9b96608bc67c4b30db88e0a5a6c6390213f7acbe1153ff6d23ce57380104288ae19373ef@'$1':21000?discport=0&raftport=50401",
-  "enode://3d9ca5956b38557aba991e31cf510d4df641dce9cc26bfeb7de082f0c07abb6ede3a58410c8f249dabeecee4ad3979929ac4c7c496ad20b8cfdd061b7401b4f5@'$2':21003?discport=0&raftport=50404"
+  "enode://3d9ca5956b38557aba991e31cf510d4df641dce9cc26bfeb7de082f0c07abb6ede3a58410c8f249dabeecee4ad3979929ac4c7c496ad20b8cfdd061b7401b4f5@'$2':21003?discport=0&raftport=50404",
+  "enode://239c1f044a2b03b6c4713109af036b775c5418fe4ca63b04b1ce00124af00ddab7cc088fc46020cdc783b6207efe624551be4c06a994993d8d70f684688fb7cf@'$3':21006?discport=0&raftport=50407"
 ]' > permissioned-nodes.json
 
 echo "[*] Cleaning up temporary data directories"
